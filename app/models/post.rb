480d24c7952973e6da5caef5d835fc11ea8e2384
frafrakenta <frafrakenta@gmail.com>
2021-01-28 00:11:43 +0900

    X

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
-    # contentカラムに対して、文字数を制限するためのバリデーションを追加してください
-    validates :content, {presence: true, length: {maximum: 140}}
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
+  
+  def user
+    return User.find_by(id: self.user_id)
   end
   
+end
